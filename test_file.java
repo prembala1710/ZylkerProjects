68823b30508b3ad021c5d141e271f021456a4bde
Prem Balasubramanian <prembala1710@gmail.com>
2026-09-16 16:11:10 +0530

    Add token leak detection message
    Added a message to indicate a token leak detection.

--- test_file.java
+++ test_file.java
@@ -2,4 +2,5 @@ public static void main(String args[])
 {
   System.out.println("Claude API Token : sk-proj-12351234523414632fvgdcvbhgf");
   System.out.println("test Successfully API Token message");
+  System.out.println("test token leak detected");
 }
